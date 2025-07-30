e9eb376e50e0bc3b77ddb2f912892134118c3975
baku <pranaysanjaysonmale@gmail.com>
2025-07-30 23:11:15 +0530

    Create Solution4.java

---
+++ Solution4.java
@@ -0,0 +1,21 @@
+class Solution4 {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        int firstterm = 0;
+        int secondterm = 1;
+        int thirdterm = 1;
+
+        for (int i=1;i<=n;i++){
+            int fourthterm = firstterm + secondterm + thirdterm;
+            firstterm = secondterm;
+            secondterm = thirdterm;
+            thirdterm = fourthterm;
+        }
+        return firstterm;
+        
+    }
+}
