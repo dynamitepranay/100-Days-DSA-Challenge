67b1449c0e723b6ba36f7e6b0b2721bc2682f9cb
baku <pranaysanjaysonmale@gmail.com>
2025-07-31 22:19:47 +0530

    Update Solution5.java

--- Solution5.java
+++ Solution5.java
@@ -1,21 +1,14 @@
 class Solution5 {
-    public int tribonacci(int n) {
-        if(n==0){
-            return 0;
-        }else if(n==1){
-            return 1;
-        }
-        int firstterm = 0;
-        int secondterm = 1;
-        int thirdterm = 1;
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while(numBottles>=numExchange){
+            int remBottles = numBottles % numExchange;
+            int newBottles = numBottles / numExchange;
+            ans = ans + newBottles;
+            numBottles = newBottles + remBottles;
 
-        for (int i=1;i<=n;i++){
-            int fourthterm = firstterm + secondterm + thirdterm;
-            firstterm = secondterm;
-            secondterm = thirdterm;
-            thirdterm = fourthterm;
         }
-        return firstterm;
+        return ans;3
         
     }
 }
